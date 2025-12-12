18b440b5ac6e3984fdc43960e74ea04214124715
Danel Torchiari <danel.t@hotmail.com>
2025-12-12 16:42:29 -0300

    fix tailwind

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
     return (
-        <div className="p-6">
-            ABOUT PAGE
-        </div>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-indigo-600">Tailwind v4 ✅</h1>
+    </div>
     );
 }
